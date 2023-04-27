5adb8bf7a9b57c7f04da6a3acacac51a8109e327
Suddala Omkar <77642195+Omkar-royal@users.noreply.github.com>
2023-04-27 05:56:53 +0000

    Pending changes exported from your codespace

---
+++ src/components/Glob4.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+function Glob4() {
+    return (
+        <div className='container4'>
+
+        
+            <div className='dd1'>
+                <h2>Most <span  class="hover-underline-animation">popular affordable pricing</span> per 
+                 jurisdictions are brought to you to       
+                  kick off your adventure.
+                </h2>
+
+            </div>
+
+        </div>
+  
+    );
+}
+
+export default Glob4;
